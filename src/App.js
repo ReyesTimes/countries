d7f3214822af18be10697d27b22ccf3d8793e376
reyestimesbts <juanr@bluetrailsoft.com>
2021-07-13 14:47:06 -0500

    add flow and style components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// @flow
 
-function App() {
+import React from "react";
+
+import "./App.css";
+
+function App(): React$Node {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
